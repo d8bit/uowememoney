e412833e169e24554e5ca0cf690516fff47adc14
Deibit <doliva@refineriaweb.com>
2016-10-02 16:01:28 +0200

    Changed title.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,9 +27,7 @@
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                <a class="navbar-brand" href="{{ url('/') }}">YouOwnMeMoney</a>
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
